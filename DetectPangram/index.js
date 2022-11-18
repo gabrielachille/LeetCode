e329791e1fd8c342e993dd6c90b798734b82902e
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2022-11-18 16:12:08 -0500

    updated index.js

--- DetectPangram/index.js
+++ DetectPangram/index.js
@@ -3,14 +3,16 @@
 //ex "The quick brown fox jumps over the lazy dog"
 //case insensitive
 
-function isPangram(string) {
-  let alpha = "abcdefghijklmnopqrstuvwxyz".split("");
-  string = string.toLowerCase();
-  return alpha.every((x) => string.includes(x));
-}
+// function isPangram(string) {
+//   let alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+//   string = string.toLowerCase();
+//   return alpha.every((x) => string.includes(x));
+// }
 
 isPangram("hello");
 //false
 
 isPangram("the quick brown fox jumps over the lazy dog");
 //true
+
+function isPangram(string) {}
