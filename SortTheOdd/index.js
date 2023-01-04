58086efe1ac88f02e089768fc19cfec53ce68e29
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-01-03 22:57:14 -0500

    updated index.js

--- SortTheOdd/index.js
+++ SortTheOdd/index.js
@@ -3,18 +3,28 @@
 //while keeping the even numbers the same
 //6kyu
 
-function sortArray(array) {
-  let odd = array.filter((x) => x % 2 === 1).sort((a, b) => a - b);
+// function sortArray(array) {
+//   let odd = array.filter((x) => x % 2 === 1).sort((a, b) => a - b);
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 !== 0) {
-      array[i] = odd.shift();
-    }
-  }
-  //   return array.map((x) => (x % 2 !== 0 ? odd.shift() : x));
-  return array;
-}
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 !== 0) {
+//       array[i] = odd.shift();
+//     }
+//   }
+//   return array;
+// }
 
 console.log(sortArray([7, 1]));
 console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
 console.log(sortArray([1, 11, 2, 8, 3, 4, 5]));
+
+function sortArray(arr) {
+  let odd = arr.filter((x) => x % 2 === 1).sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 1) {
+      arr[i] = odd.shift();
+    }
+  }
+  return arr;
+}
