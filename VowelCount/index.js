7aeb449427293de0d66c9cce0adc2ec701fbf332
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-05-10 15:57:58 -0400

    updated index.js

--- VowelCount/index.js
+++ VowelCount/index.js
@@ -1,16 +1,16 @@
 //7 kyu
 //Count the number of vowels in the string
 
-// function getVowelCount(str) {
-//   let vowel = /[aeiou]/gi;
-//   let count = 0;
-//   for (let i = 0; i < str.length; i++) {
-//     if (str[i].match(vowel)) {
-//       count++;
-//     }
-//   }
-//   console.log(count);
-//   return count;
-// }
+function getVowelCount(str) {
+  let vowel = /[aeiou]/gi;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(vowel)) {
+      count++;
+    }
+  }
+  console.log(count);
+  return count;
+}
 
 getVowelCount("Hello World");
