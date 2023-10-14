50c4fb70d8bbad752491f12e5c1f0b69139936ed
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-13 23:12:33 -0400

    Add files via upload

---
+++ TenMinutesWalk/index.js
@@ -0,0 +1,8 @@
+function isValidWalk(walk) {
+  directions = { n: 1, e: 1, w: -1, s: -1 };
+
+  for (let i = 0; i < walk.length; i++) {}
+  return walk;
+}
+
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
