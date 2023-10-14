fee0608ea9207c44c4696b7330312d5323e0a14e
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-14 15:15:50 -0400

    Update index.js

--- TenMinutesWalk/index.js
+++ TenMinutesWalk/index.js
@@ -1,8 +1,25 @@
 function isValidWalk(walk) {
-  directions = { n: 1, e: 1, w: -1, s: -1 };
+  // directions = { n: 1, e: 1, w: -1, s: -1 };
+  let count = 0;
+  if (walk.length != 10) {
+    return false;
+  }
 
-  for (let i = 0; i < walk.length; i++) {}
-  return walk;
+  for (let i = 0; i < walk.length; i++) {
+    if (i === "n" || i === "e") {
+      count++;
+      console.log(count);
+    } else if (i === "s" || i === "w") {
+      count--;
+      console.log(count);
+    }
+  }
+
+  return count === 0 ? true : false;
 }
 
-console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+// console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+console.log(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]));
+// console.log(
+//   isValidWalk(["w", "s", "n", "e", "s", "e", "w", "n", "w", "e", "w", "e"])
+// );
