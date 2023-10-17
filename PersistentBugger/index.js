4f6548db5968fab11d5dcd7975bc7608dfefdca8
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-17 17:50:12 -0400

    Add files via upload

---
+++ PersistentBugger/index.js
@@ -0,0 +1,11 @@
+//multiply number until
+
+function persistence(num) {
+  let numstring = [];
+  count = 0;
+  numstring = num.toString().split("");
+  //map conversion from string to digits
+  console.log(numstring);
+}
+
+persistence(3456779);
