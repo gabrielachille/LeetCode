333d9b706dd4ed5e20810c78e0a7803914fa685e
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-19 18:06:45 -0400

    Update index.js

--- PersistentBugger/index.js
+++ PersistentBugger/index.js
@@ -1,11 +1,17 @@
-//multiply number until
+//multiply number until it's a single digit
 
+count = 0;
 function persistence(num) {
   let numstring = [];
-  count = 0;
   numstring = num.toString().split("");
+  if (numstring.length > 1) {
+  } else {
+    return count;
+  }
   //map conversion from string to digits
-  console.log(numstring);
+  let bitmap = numstring.map((x) => parseInt(x, 10));
+  let multiply = bitmap.reduce((a, b) => a * b);
+  returnpersistence(multiply);
 }
 
-persistence(3456779);
+console.log(persistence(3456779));
