f2f6b88cc632693b530422dde201ccd12dc54846
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-21 17:44:31 -0400

    Update index.js

--- PersistentBugger/index.js
+++ PersistentBugger/index.js
@@ -1,17 +1,15 @@
-//multiply number until it's a single digit
-
-count = 0;
 function persistence(num) {
-  let numstring = [];
-  numstring = num.toString().split("");
-  if (numstring.length > 1) {
-  } else {
-    return count;
+  let count = 0;
+  // turn digits into string
+  let numstring = String(num);
+  while (numstring.length > 1) {
+    //use spread operator to turn string into array
+    //use reduce to multiply different numbers to one number
+    //don't worry about turning stringed numbers back to digits because of type conversion
+    // rinse and repeat until single digit
+    numstring = String([...numstring].reduce((prev, curr) => prev * curr));
+    count++;
   }
-  //map conversion from string to digits
-  let bitmap = numstring.map((x) => parseInt(x, 10));
-  let multiply = bitmap.reduce((a, b) => a * b);
-  returnpersistence(multiply);
-}
 
-console.log(persistence(3456779));
+  return count;
+}
