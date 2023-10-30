96ae9a6e7e59dbdbd633044201e075ee49e496a7
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-10-30 17:25:14 -0400

    Update index.js

--- CountingDuplicates/index.js
+++ CountingDuplicates/index.js
@@ -1,22 +1,27 @@
 function duplicateCount(text) {
-  let obj = {};
+  // iterate through string and return the amount of repeated characters
   let count = 0;
+  let obj = {};
 
   for (let i of text) {
+    //creates a charater key for every new character,
+    //and sets its value to 1
     i = i.toLowerCase();
     if (!obj[i]) {
       obj[i] = 1;
     } else {
+      //if there is a repeated character it adds +1 to the value
       obj[i]++;
     }
-    for(let i in obj){
-    if (obj[i] > 1){
-      count++
-    }
-    }
-    return count;
   }
-  return obj;
+  //counts how many characters appear more than once
+  for (let i in obj) {
+    if (obj[i] > 1) {
+      //if a value is more than 1 count goes up by 1
+      count++;
+    }
+  }
+  return count;
 }
 
-duplicateCount("SmartWatch");
+console.log(duplicateCount("aabbcde"));
