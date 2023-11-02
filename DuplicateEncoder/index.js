879aec604fe27d654ce2d4e57dd6358cec8cf2a8
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-02 00:34:22 -0400

    Update index.js

--- DuplicateEncoder/index.js
+++ DuplicateEncoder/index.js
@@ -1,16 +1,19 @@
 function duplicateEncode(word) {
   // take given string and return "(" for characters that only appear once
   // and ")" for characters that appear multiple times
-  //   word.map(char)=>
   word = word.toLowerCase();
   word = word.split("");
   for (let i = 0; i < word.length; i++) {
+    // figure out the last character egdecase
     if (word.indexOf(word[i]) === word.lastIndexOf(word[i])) {
-      word[i] == "(";
+      word[i] = "(";
     } else {
-      word[i] == ")";
+      word[i] = ")";
     }
   }
+  console.log(word);
 }
 
 duplicateEncode("recede");
+
+//   word.map(char)=>
