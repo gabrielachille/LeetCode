d839e5c1c2f9063161069133357d682615926839
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-11 19:54:32 -0500

    Added local file

---
+++ DetectPangram/index.js
@@ -0,0 +1,8 @@
+//Create function to detect is string uses every word
+//in the alphabet
+//ex "The quick brown fox jumps over the lazy dog"
+//case insensitive
+
+function isPangram(string) {
+  //...
+}
