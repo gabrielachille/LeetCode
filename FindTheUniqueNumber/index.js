ec4401669bd33f7c504557df7ed31ab71bc6a0e9
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-17 18:53:29 -0500

    updated index.js

--- FindTheUniqueNumber/index.js
+++ FindTheUniqueNumber/index.js
@@ -1,6 +1,8 @@
 //Every number in an array is the same exept for one
 //Return the one that isn't
 
-function findUniq(arr) {
-  return arr.find((n) => indexOf(n) === arr.lastIndexOf(n));
-}
+// function findUnique(arr) {
+//   return arr.find((n) => indexOf(n) === arr.lastIndexOf(n));
+// }
+
+function findUnique(arr) {}
