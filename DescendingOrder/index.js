193c5c3faaab209ae65712d2e041375f13f301da
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-23 00:21:34 -0500

    solved Descending Order

--- DescendingOrder/index.js
+++ DescendingOrder/index.js
@@ -2,8 +2,12 @@
 //7 kyu
 
 function descendingOrder(n) {
-  n = n.toString().split("");
-  return n;
+  n = n
+    .toString()
+    .split("")
+    .sort((x, y) => y - x)
+    .join("");
+  return +n;
 }
 
 console.log(descendingOrder(145263));
