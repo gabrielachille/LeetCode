4aeb03ba3095ef09c75f4429f688a9997c3e23f2
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-25 11:47:25 -0500

    updated index.js

--- DescendingOrder/index.js
+++ DescendingOrder/index.js
@@ -1,13 +1,17 @@
 //take integer and rearrange from highest to lowest
 //7 kyu
 
-function descendingOrder(n) {
-  n = n
-    .toString()
-    .split("")
-    .sort((x, y) => y - x)
-    .join("");
-  return +n;
-}
+// function descendingOrder(n) {
+//   n = n
+//     .toString()
+//     .split("")
+//     .sort((x, y) => y - x)
+//     .join("");
+//   return +n;
+// }
 
 console.log(descendingOrder(145263));
+
+function descendingOrder(num) {
+  return num.toString().split("").sort().reverse().join("");
+}
