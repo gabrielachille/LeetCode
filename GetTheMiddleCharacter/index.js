215e0f3f1c535d6f85d137ea5574522cae31394d
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-27 23:01:20 -0500

    solved Get the middle character

--- GetTheMiddleCharacter/index.js
+++ GetTheMiddleCharacter/index.js
@@ -1,3 +1,11 @@
+//return middle character/s of a word
+
 function getMiddle(s) {
-  //Code goes here!
+  return s.length % 2 == 1
+    ? s[Math.floor(s.length / 2)]
+    : s[s.length / 2 - 1] + s[s.length / 2];
 }
+
+console.log(getMiddle("riddle"));
+
+console.log(getMiddle("testing"));
