4e504ecb5c5665fba11ab28b9ad74472328a8a8c
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-11-29 17:57:01 -0500

    optimized Mumbling

--- Mumbling/index.js
+++ Mumbling/index.js
@@ -1,8 +1,21 @@
-function accum(s) {
-  arr = "";
-  s = s.toUpperCase().split("");
-  for (let i = 0; i < s.length; i++) {}
-  return s;
-}
+// function accum(s) {
+//   num = "";
+//   let str = s[0].toUpperCase();
+//   for (let i = 1; i < s.length; i++) {
+//     num += "-" + s[i].toUpperCase();
+//     for (let j = 0; j < i; j++) {
+//       num += s[i].toLowerCase();
+//     }
+//   }
+//   return str + num;
+// }
 
 console.log(accum("abCdE"));
+
+function accum(s) {
+  s = s
+    .split("")
+    .map((num, x) => num.toUpperCase() + num.toLowerCase().repeat(x))
+    .join("-");
+  return s;
+}
