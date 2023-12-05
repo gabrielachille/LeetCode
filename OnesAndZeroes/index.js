b8c77612a52f4caeb21e1b687e33800de1e97fcb
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-12-05 17:49:03 -0500

    solved ones and zeroes

---
+++ OnesAndZeroes/index.js
@@ -0,0 +1,11 @@
+//convert binary array to number digit
+//7kyu
+
+const binaryArrayToNumber = (arr) => {
+  return parseInt(arr.join(""), 2);
+};
+
+console.log(binaryArrayToNumber([0, 0, 0, 1]));
+console.log(binaryArrayToNumber([0, 0, 1, 0]));
+console.log(binaryArrayToNumber([1, 0, 0, 1]));
+console.log(binaryArrayToNumber([1, 1, 1, 1]));
