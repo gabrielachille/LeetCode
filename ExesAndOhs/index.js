f052aa77ab7d0fc0a4ce62aff084d9d48df521ce
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-12-06 22:46:37 -0500

    updated index.js

--- ExesAndOhs/index.js
+++ ExesAndOhs/index.js
@@ -1,3 +1,29 @@
+//return true if the number of x's and o's are the same
+//ignore casing
+//loop through the string with 2 count variables
+//update each with respective x or o
+//return true/false both counts equal eachother
+//7kyu
+
 function XO(str) {
-  //code here
+  //let countX = "x";
+  // let countO = "o";
+
+  //   for (i = 0; i < str.length; i++) {
+  //     if (str[i] === "x") {
+  //       countX++;
+  //     }
+  //     if (str[i] === "o") {
+  //       countO++;
+  //     }
+  //   }
+  //   return countO === countX;
+  str = str.toLowerCase().split("");
+  return (
+    str.filter((x) => x.match(/x/gi)).length ===
+    str.filter((x) => x.match(/o/gi)).length
+  );
 }
+
+console.log(XO("ooxx"));
+console.log(XO("xooxx"));
