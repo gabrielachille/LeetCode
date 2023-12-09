18e3a83ebbc71702f8e64d521e56cb2cd393016c
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-12-08 21:37:23 -0500

    updated index.js

---
+++ StopSpinningMyWords/index.js
@@ -0,0 +1,26 @@
+//write function that reverses every word with five
+//or more characters in a string
+//6kyu
+
+function spinWords(string) {
+  string = string.split(" ");
+
+  for (i = 0; i < string.length; i++) {
+    if (string[i].length >= 5) {
+      string[i] = string[i].split("").reverse().join("");
+    }
+  }
+
+  return string.join(" ");
+}
+
+console.log(spinWords("Hey fellow warriors"));
+console.log(spinWords("This is a test"));
+console.log(spinWords("This is another test"));
+
+// function spinWords(string) {
+//   return string
+//     .split(" ")
+//     .map((x) => (x.length > 4 ? x : x.split("").reverse().join("")))
+//     .join("");
+// }
