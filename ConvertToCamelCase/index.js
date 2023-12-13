0124cf9528b0b77b88d1e9af49883bed54fcf9be
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-12-13 15:41:02 -0500

    updated index.js

--- ConvertToCamelCase/index.js
+++ ConvertToCamelCase/index.js
@@ -1,8 +1,13 @@
 //Convert strings with dash or underscore
 //and return camelCase strings
+//6kyu
 
 function toCamelCase(str) {
-  let dash = /-_/gi;
-  let str = str.split(dash);
+  str = str.split(/-|_/g);
   return str;
+  //planned- str.map(if not first item in array then firstletter.touppercase())
 }
+
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("the_Stealth_Warrior"));
+console.log(toCamelCase("the_stealth-warrior"));
