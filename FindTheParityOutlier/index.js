14edc6e84781ec1b6e3ac376c385887d158dbbc3
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2023-12-24 00:49:20 -0500

    updated index.js

--- FindTheParityOutlier/index.js
+++ FindTheParityOutlier/index.js
@@ -1,13 +1,3 @@
 function findOutlier(integers) {
-  let count = 0;
-  for (let i = 0; i < integers.length; i++) {
-    for (let j = 0; j < integers.length; j++) {
-      if (integers[i] % 2 === 0) {
-        count++;
-      }
-    }
-    if (count > 1) {
-      return "Odd";
-    }
-  }
+  return findOutlier((x, y, z) => 0);
 }
