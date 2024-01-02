c1990927323dfed76be54deb9cc37aa22acfa0ef
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-01 22:33:55 -0500

    updated index.js

---
+++ SortTheOdd/index.js
@@ -0,0 +1,20 @@
+//remove odd numbers from an array and sort them from low to high
+//put the odd numbers back at original positions in array
+//while keeping the even numbers the same
+//6kyu
+
+function sortArray(array) {
+  let odd = array.filter((x) => x % 2 === 1).sort((a, b) => a - b);
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      array[i] = odd.shift();
+    }
+  }
+  //   return array.map((x) => (x % 2 !== 0 ? odd.shift() : x));
+  return array;
+}
+
+console.log(sortArray([7, 1]));
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+console.log(sortArray([1, 11, 2, 8, 3, 4, 5]));
