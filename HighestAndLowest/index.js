0306bbabc1cce924e3a6b0b455f294ff953ca206
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-03 23:37:17 -0500

    updated index.js

--- HighestAndLowest/index.js
+++ HighestAndLowest/index.js
@@ -1,11 +1,15 @@
 //given string of numbers separated by a space
 //return a string of the highest number and the lowest number
 
-function highAndLow(num) {
-  num = num.split(" ").map((x) => parseInt(x, 10));
-  let high = Math.max(...num);
-  let low = Math.min(...num);
-  return `${high} ${low}`.toString();
-}
+// function highAndLow(num) {
+//   num = num.split(" ").map((x) => parseInt(x, 10));
+//   let high = Math.max(...num);
+//   let low = Math.min(...num);
+//   return `${high} ${low}`.toString();
+// }
 
 console.log(highAndLow("1 2 3 4 5"));
+
+function highAndLow(str) {
+  str = str.split(" ");
+}
