7aa7546c577c9477b8c5815e3e5fc24579a08b9d
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-16 16:36:56 -0500

    added Simple Pig Latin

---
+++ SimplePigLatin/index.js
@@ -0,0 +1,12 @@
+function pigIt(str) {
+  str = str
+    .split(" ")
+    .map((x) => (x.match(/[a-z]/i) ? x.slice(1) + x.charAt(0) + "ay" : x))
+    .join(" ");
+
+  //   return str.map((x) => x.match(punct) === false ?);
+  return str;
+}
+
+console.log(pigIt("Pig latin is cool"));
+console.log(pigIt("Hello world !"));
