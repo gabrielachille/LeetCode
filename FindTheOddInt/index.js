845a5f4d24a411ffa744fe321629d793b80548eb
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-18 17:37:18 -0500

    updated index.js

--- FindTheOddInt/index.js
+++ FindTheOddInt/index.js
@@ -1,11 +1,30 @@
 //Given an array of numbers find the number
 //that appears an odd number of times.
 
+// function findOdd(A) {
+//   let count = 0;
+//   for (let i = 0; i < A.length; i++) {
+//     for (let j = 0; j < A.length; j++) {
+//       if (A[j] === A[i]) {
+//         count++;
+//       }
+//     }
+//     if (count % 2 === 1) {
+//       return A[i];
+//     }
+//   }
+// }
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+
 function findOdd(A) {
   let count = 0;
+
   for (let i = 0; i < A.length; i++) {
     for (let j = 0; j < A.length; j++) {
-      if (A[j] === A[i]) {
+      if (A[i] === A[j]) {
         count++;
       }
     }
@@ -14,7 +33,3 @@ function findOdd(A) {
     }
   }
 }
-
-console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
-console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
-console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
