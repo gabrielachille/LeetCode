9f77a02aa685cd26821eb6775178ac4ca75e7aea
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-19 23:23:15 -0500

    updated index.js

--- ArrayDiff/index.js
+++ ArrayDiff/index.js
@@ -1,10 +1,14 @@
 //remove any instances of the number in the second array
-//from nthe first array
+//from the first array
 //6kyu
+
+// function arrayDiff(a, b) {
+//   return a.filter((x) => !b.includes(x));
+// }
+
+console.log(arrayDiff([1, 2], [2]));
+console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
 
 function arrayDiff(a, b) {
   return a.filter((x) => !b.includes(x));
 }
-
-console.log(arrayDiff([1, 2], [2]));
-console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
