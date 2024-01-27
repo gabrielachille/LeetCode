e37bff9a68caedc19d11f32a2756b5c7e80dd127
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-01-26 22:58:46 -0500

    updated index.js

--- HighestAndLowest/index.js
+++ HighestAndLowest/index.js
@@ -17,4 +17,7 @@ console.log(highAndLow("1 2 3 4 5"));
 
 function highAndLow(num) {
   num = num.split(" ").map((x) => parseInt(x, 10));
+  high = num.reduce((x, y) => (x > y ? x : y));
+  low = num.reduce((x, y) => (x < y ? x : y));
+  return `${high} ${low}`;
 }
