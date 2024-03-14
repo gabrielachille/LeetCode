baa3fe63512ff65cc34c1d6a69ee589e222b0268
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-03-14 15:17:50 -0400

    updated solution.py

--- YoureASquare/solution.py
+++ YoureASquare/solution.py
@@ -1,6 +1,19 @@
 #return true if square
 # print("i am iron man", "\ni am iron, man", "\nman iron am i ?")
 
-name = input("what is your name? ")
+# name = input("what is your name? ")
 
-print("Hello "+ name)
+# print("Hello "+ name)
+
+#python madlibs
+
+#-- went to the store and grabbed some --, then i went home and played --
+
+name = input("who?")
+
+item = input("what do you need?")
+
+game = input("What's your favorite game?")
+
+
+print(name + " went to the store and bought " + item +" then they went home and played " + game)
