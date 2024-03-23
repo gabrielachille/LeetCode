9743aff36a59276bbc55f112ca4c20542368a176
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-03-23 19:00:27 -0400

    updated solution.py

--- YoureASquare/solution.py
+++ YoureASquare/solution.py
@@ -9,11 +9,21 @@
 
 #-- went to the store and grabbed some --, then i went home and played --
 
-name = input("who?")
+# name = input("who?")
 
-item = input("what do you need?")
+# item = input("what do you need?")
 
-game = input("What's your favorite game?")
+# game = input("What's your favorite game?")
 
+# print(name + " went to the store and bought " + item +" then they went home and played " + game)
 
-print(name + " went to the store and bought " + item +" then they went home and played " + game)
+price = 7
+
+numcoffee = input("RB: How many coffees do you wnat?")
+total = price * int(numcoffee)
+
+total = str(total)
+
+print("RB: Your total is $" + total)
+
+# print(total)
