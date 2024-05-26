63654cf3fa4b89b3b54aa9acba3d61b439aaa21a
gabrielachille <82066891+gabrielachille@users.noreply.github.com>
2024-05-25 20:44:09 -0400

    updated solution.py

--- YoureASquare/solution.py
+++ YoureASquare/solution.py
@@ -5,14 +5,14 @@
 #python madlibs
 
 #-- went to the store and grabbed some --, then i went home and played --
-1
-name = input("who dis?")
+# 1
+# name = input("who dis?")
 
-item = input("what do you need?")
+# item = input("what do you need?")
 
-game = input("What's your favorite game?")
+# game = input("What's your favorite game?")
 
-print(name + " went to the store and bought " + item +" then they went home and played " + game)
+# print(name + " went to the store and bought " + item +" then they went home and played " + game)
 
 # price = 8
 
@@ -25,10 +25,22 @@ print(name + " went to the store and bought " + item +" then they went home and 
 
 # print(total)
 
-age = int(input("Enter your age\n"))
+# age = int(input("Enter your age\n"))
 
-if age >= 21:
-    print("You get a beer")
-    if age < 21:
+# if age >= 21:
+#     print("You get a beer")
+#     if age < 21:
+# else:
+#     print("You get a juice")
+
+name = input("What is your name?\n")
+
+if name == "Ben" or name == "Patricia":
+    evil_status = input("Are you evil?\n")
+    if evil_status == "Yes" and name == "Ben":
+        print("Your're not welcome here Ben!! Get out!!")
+    if evil_status == "Yes" and name == "Patricia":
+        print("You're not welcome here Patricia!! Get out!! ")
+        exit()
 else:
-    print("You get a juice")
+    print("Hello " + name + ", thank you so much for coming in today.\n\n\n")
